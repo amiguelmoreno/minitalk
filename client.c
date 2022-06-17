cea4ef0d697bf1bdd657a3c42e10a62e1b5353af
Antonio Miguel Moreno Molina <migue@Antonios-MacBook-Pro.local>
2022-06-17 10:49:13 +0200

    cambios minitalk

---
+++ client.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antmoren <antmoren@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 16:38:44 by antmoren          #+#    #+#             */
+/*   Updated: 2022/06/15 20:02:42 by antmoren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf/includes/ft_printf.h"
+#include "../ft_printf/libft/libft.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	send_bit(int pid, char *str, size_t len)
+{
+	int		shift;
+	size_t	i;
+
+	i = 0;
+	while (i <= len)
+	{
+		shift = 0;
+		while (shift < 7)
+		{
+			if ((str[i] >> shift) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			shift++;
+			usleep(300);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*str;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		send_bit(pid, str, ft_strlen(str));
+	}
+	else
+		ft_printf("\nYOU EITHER LEFT IT BLANK OR ARE DOING MORE THAN 1 WORD\n\n");
+}
